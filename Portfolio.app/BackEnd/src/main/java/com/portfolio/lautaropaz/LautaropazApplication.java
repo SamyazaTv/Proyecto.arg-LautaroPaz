e74e341ab7c64c291b5341a4da80477ceab9b753
Samyaza ( Lautaro Paz ) <lautaropaz__@hotmail.com>
2022-10-29 05:11:30 -0300

    Cambios en Front end

---
+++ Portfolio.app/BackEnd/src/main/java/com/portfolio/lautaropaz/LautaropazApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.lautaropaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LautaropazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LautaropazApplication.class, args);
+	}
+
+}
---
+++ Portfolio.app/BackEnd/src/test/java/com/portfolio/lautaropaz/LautaropazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.lautaropaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LautaropazApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
